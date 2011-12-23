075a8a0868e6ff356661d4092a35e1f77b11b221
Iurii Kriachko <iurii.kriachko@gmail.com>
2011-12-23 17:38:15 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-bbcode',
+      version='1.0',
+      description='django-bbcode',
+      packages=['bbcode/', ],
+     )
